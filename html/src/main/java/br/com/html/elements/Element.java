dfb58b43c003b896e1c93b088a51964f3b60be5c
Gustavo Coelho <gutorc@hotmail.com>
2015-07-25 01:44:00 -0300

    Modification of methods
    Moving methods addElement and buildInsideElement
    to abstract class.
    
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

--- html/src/main/java/br/com/html/elements/Element.java
+++ html/src/main/java/br/com/html/elements/Element.java
@@ -1,11 +1,15 @@
 package br.com.html.elements;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.com.html.HtmlElement;
 
 public abstract class Element implements HtmlElement {
 	
 	protected String id;
 	protected String html;
+	protected List<HtmlElement> elements;
 	
 	protected String getId(){
 		return "id=\"" + id + "\"";
@@ -13,6 +17,23 @@ public abstract class Element implements HtmlElement {
 	
 	public void setId(String id){
 		this.id = id;
+	}
+	
+	protected String buildInsideElements() {
+		String htmlInside = new String();
+		if(elements != null){
+			for(HtmlElement element : elements){
+				htmlInside += element.build();
+			}
+		}
+		return htmlInside;
+	}
+	
+	public void addElement(HtmlElement element){
+		if(elements == null){
+			elements = new ArrayList<HtmlElement>();
+		}
+		elements.add(element);
 	}
 
 }
--- html/src/main/java/br/com/html/elements/Link.java
+++ html/src/main/java/br/com/html/elements/Link.java
@@ -7,7 +7,7 @@ import br.com.html.HtmlElement;
 public class Link extends Element {
 	
 	private String href;
-	private List<HtmlElement> elements;
+	
 
 	public String build() {
 		html = "<a " + getId();
@@ -19,22 +19,8 @@ public class Link extends Element {
 		return html;
 	}
 
-	private String buildInsideElements() {
-		String htmlInside = new String();
-		if(elements != null){
-			for(HtmlElement element : elements){
-				htmlInside += element.build();
-			}
-		}
-		return htmlInside;
-	}
-	
 	public void setHref(String href){
 		this.href = href;
 	}
 	
-	public void addElement(HtmlElement element){
-		elements.add(element);
-	}
-
 }
