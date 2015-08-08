92e25fd148ad4e46401f9644ae2d7c8a72c3c202
Gustavo Coelho <gutorc@hotmail.com>
2015-08-07 23:20:09 -0300

    Email Element
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

---
+++ html/src/main/java/br/com/html/body/elements/input/Email.java
@@ -0,0 +1,21 @@
+package br.com.html.body.elements.input;
+
+public class Email extends Input {
+
+	public Email(String id, String value, String text) {
+		super(EMAIL, id, value, text);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Email(String id, String value) {
+		super(EMAIL, id, value);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Email(String id) {
+		super(EMAIL, id);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
---
+++ html/src/test/java/br/com/html/body/elements/input/EmailTest.java
@@ -0,0 +1,47 @@
+package br.com.html.body.elements.input;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.html.body.elements.GenerateIds;
+
+public class EmailTest {
+
+	Email email;
+	
+	@Before
+	public void setUp(){
+		
+	}
+	
+	@Test
+	public void testBuildWithoutArguments() {
+		String id = GenerateIds.nextID();
+		email = new Email(id);
+		String expected = "<input id=\"" + id + "\" type=\"email\">";
+		assertEquals(expected, email.build());
+	}
+	
+	@Test
+	public void testBuildWithValue() {
+		String id = GenerateIds.nextID();
+		email = new Email(id,"Test");
+		String expected = "<input id=\"" + id + "\" type=\"email\" "
+				+ "value=\"Test\">";
+		System.out.println(email.build());
+		assertEquals(expected, email.build());
+	}
+	
+	@Test
+	public void testBuildWithValueAndText() {
+		String id = GenerateIds.nextID();
+		email = new Email(id,"Test","Doing test");
+		String expected = "<input id=\"" + id + "\" type=\"email\" "
+				+ "value=\"Test\">Doing test";
+		System.out.println(email.build());
+		assertEquals(expected, email.build());
+	}
+
+}
